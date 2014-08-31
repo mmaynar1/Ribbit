e3f5f405a65ca06247a7896b00e05b6b871855a7
Mitch Maynard <Mitch.Maynard@abcfinancial.com>
2014-08-31 15:16:12 -0500

    Added ability to log out

--- app/src/main/java/com/mitchum/maynard/ribbit/MainActivity.java
+++ app/src/main/java/com/mitchum/maynard/ribbit/MainActivity.java
@@ -23,7 +23,8 @@ import com.parse.ParseAnalytics;
 import com.parse.ParseUser;
 
 
-public class MainActivity extends Activity implements ActionBar.TabListener {
+public class MainActivity extends Activity implements ActionBar.TabListener
+{
 
 
     public static final String TAG = MainActivity.class.getSimpleName();
@@ -43,19 +44,17 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
     ViewPager mViewPager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
 
         ParseAnalytics.trackAppOpened( getIntent() );
 
         ParseUser currentUser = ParseUser.getCurrentUser();
-        if( currentUser == null)
+        if ( currentUser == null )
         {
-            Intent intent = new Intent( this, LoginActivity.class );
-            intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
-            intent.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TASK );
-            startActivity( intent );
+            navigateToLogin();
         }
         else
         {
@@ -64,107 +63,131 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 
         // Set up the action bar.
         final ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setNavigationMode( ActionBar.NAVIGATION_MODE_TABS );
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
+        mSectionsPagerAdapter = new SectionsPagerAdapter( getFragmentManager() );
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager = (ViewPager) findViewById( R.id.pager );
+        mViewPager.setAdapter( mSectionsPagerAdapter );
 
         // When swiping between different sections, select the corresponding
         // tab. We can also use ActionBar.Tab#select() to do this if we have
         // a reference to the Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+        mViewPager.setOnPageChangeListener( new ViewPager.SimpleOnPageChangeListener()
+        {
             @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
+            public void onPageSelected( int position )
+            {
+                actionBar.setSelectedNavigationItem( position );
             }
-        });
+        } );
 
         // For each of the sections in the app, add a tab to the action bar.
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++)
+        {
             // Create a tab with text corresponding to the page title defined by
             // the adapter. Also specify this Activity object, which implements
             // the TabListener interface, as the callback (listener) for when
             // this tab is selected.
             actionBar.addTab(
                     actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-                            .setTabListener(this));
+                            .setText( mSectionsPagerAdapter.getPageTitle( i ) )
+                            .setTabListener( this ) );
         }
+    }
+
+    private void navigateToLogin()
+    {
+        Intent intent = new Intent( this, LoginActivity.class );
+        intent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK );
+        intent.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TASK );
+        startActivity( intent );
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu( Menu menu )
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate( R.menu.main, menu );
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected( MenuItem item )
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if ( id == R.id.action_logout )
+        {
+           ParseUser.logOut();
+            navigateToLogin();
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected( item );
     }
 
     @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    public void onTabSelected( ActionBar.Tab tab, FragmentTransaction fragmentTransaction )
+    {
         // When the given tab is selected, switch to the corresponding page in
         // the ViewPager.
-        mViewPager.setCurrentItem(tab.getPosition());
+        mViewPager.setCurrentItem( tab.getPosition() );
     }
 
     @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    public void onTabUnselected( ActionBar.Tab tab, FragmentTransaction fragmentTransaction )
+    {
     }
 
     @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    public void onTabReselected( ActionBar.Tab tab, FragmentTransaction fragmentTransaction )
+    {
     }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    public class SectionsPagerAdapter extends FragmentPagerAdapter
+    {
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
+        public SectionsPagerAdapter( FragmentManager fm )
+        {
+            super( fm );
         }
 
         @Override
-        public Fragment getItem(int position) {
+        public Fragment getItem( int position )
+        {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            return PlaceholderFragment.newInstance( position + 1 );
         }
 
         @Override
-        public int getCount() {
+        public int getCount()
+        {
             // Show 3 total pages.
             return 3;
         }
 
         @Override
-        public CharSequence getPageTitle(int position) {
+        public CharSequence getPageTitle( int position )
+        {
             Locale l = Locale.getDefault();
-            switch (position) {
+            switch ( position )
+            {
                 case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
+                    return getString( R.string.title_section1 ).toUpperCase( l );
                 case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
+                    return getString( R.string.title_section2 ).toUpperCase( l );
                 case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
+                    return getString( R.string.title_section3 ).toUpperCase( l );
             }
             return null;
         }
@@ -173,7 +196,8 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment
+    {
         /**
          * The fragment argument representing the section number for this
          * fragment.
@@ -184,21 +208,24 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
+        public static PlaceholderFragment newInstance( int sectionNumber )
+        {
             PlaceholderFragment fragment = new PlaceholderFragment();
             Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
+            args.putInt( ARG_SECTION_NUMBER, sectionNumber );
+            fragment.setArguments( args );
             return fragment;
         }
 
-        public PlaceholderFragment() {
+        public PlaceholderFragment()
+        {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        public View onCreateView( LayoutInflater inflater, ViewGroup container,
+                                  Bundle savedInstanceState )
+        {
+            View rootView = inflater.inflate( R.layout.fragment_main, container, false );
             return rootView;
         }
     }
