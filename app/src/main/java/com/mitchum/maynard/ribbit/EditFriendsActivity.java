2a5d29f1bdb5ca4155e1fd2abcfc62c7340e0779
Mitch Maynard <Mitch.Maynard@abcfinancial.com>
2014-09-07 17:25:35 -0500

    Can add friends

--- app/src/main/java/com/mitchum/maynard/ribbit/EditFriendsActivity.java
+++ app/src/main/java/com/mitchum/maynard/ribbit/EditFriendsActivity.java
@@ -7,12 +7,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
-import com.parse.ParseUser;
+import android.widget.ListView;
+import com.parse.*;
 
 import java.util.List;
 
@@ -22,6 +21,8 @@ public class EditFriendsActivity extends ListActivity
 
     public static final String TAG = EditFriendsActivity.class.getSimpleName();
     private List<ParseUser> mUsers;
+    private ParseRelation<ParseUser> mFriendsRelation;
+    private ParseUser mCurrentUser;
 
     @Override
     protected void onCreate( Bundle savedInstanceState )
@@ -29,12 +30,18 @@ public class EditFriendsActivity extends ListActivity
         super.onCreate( savedInstanceState );
         requestWindowFeature( Window.FEATURE_INDETERMINATE_PROGRESS );
         setContentView( R.layout.activity_edit_friends );
+
+        getListView().setChoiceMode( ListView.CHOICE_MODE_MULTIPLE );
     }
 
     @Override
     protected void onResume()
     {
         super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mFriendsRelation = mCurrentUser.getRelation( ParseConstants.KEY_FRIENDS_RELATION );
+
 
         setProgressBarIndeterminateVisibility( true );
         ParseQuery<ParseUser> query = ParseUser.getQuery();
@@ -53,7 +60,7 @@ public class EditFriendsActivity extends ListActivity
                     String[] usernames = new String[mUsers.size()];
 
                     int i = 0;
-                    for(ParseUser user : mUsers)
+                    for (ParseUser user : mUsers)
                     {
                         usernames[i] = user.getUsername();
                         ++i;
@@ -61,8 +68,9 @@ public class EditFriendsActivity extends ListActivity
 
                     ArrayAdapter<String> adapter = new ArrayAdapter<String>( EditFriendsActivity.this,
                                                                              android.R.layout.simple_list_item_checked,
-                                                                           usernames);
-                    setListAdapter(adapter);
+                                                                             usernames );
+                    setListAdapter( adapter );
+                    addFriendCheckmarks();
                 }
                 else
                 {
@@ -73,6 +81,37 @@ public class EditFriendsActivity extends ListActivity
                     builder.setPositiveButton( android.R.string.ok, null );
                     AlertDialog dialog = builder.create();
                     dialog.show();
+                }
+            }
+        } );
+    }
+
+    private void addFriendCheckmarks()
+    {
+        mFriendsRelation.getQuery().findInBackground( new FindCallback<ParseUser>()
+        {
+            @Override
+            public void done( List<ParseUser> friends, ParseException e )
+            {
+                if(e == null)
+                {
+                    //list returned - look for a match
+                    for(int i = 0; i < mUsers.size(); ++i)
+                    {
+                        ParseUser user = mUsers.get( i );
+                        for(ParseUser friend : friends)
+                        {
+                            if(friend.getObjectId().equals( user.getObjectId() ))
+                            {
+                                //have a match
+                                getListView().setItemChecked( i, true );
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Log.e(TAG, e.getMessage());
                 }
             }
         } );
@@ -98,5 +137,35 @@ public class EditFriendsActivity extends ListActivity
             return true;
         }
         return super.onOptionsItemSelected( item );
+    }
+
+    @Override
+    protected void onListItemClick( ListView l, View v, int position, long id )
+    {
+        super.onListItemClick( l, v, position, id );
+
+        if ( getListView().isItemChecked( position ) )
+        {
+            //add friend
+            mFriendsRelation.add( mUsers.get( position ) );
+            mCurrentUser.saveInBackground( new SaveCallback()
+            {
+                @Override
+                public void done( ParseException e )
+                {
+                    if ( e != null )
+                    {
+                        Log.e( TAG, e.getMessage() );
+                    }
+                }
+            } );
+        }
+        else
+        {
+            //remove friend
+
+        }
+
+
     }
 }
