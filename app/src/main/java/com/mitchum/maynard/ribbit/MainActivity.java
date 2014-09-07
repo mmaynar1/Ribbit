545435cc26a4add5951ea67388d4ee69a4a0cb3a
Mitch Maynard <Mitch.Maynard@abcfinancial.com>
2014-09-07 16:36:24 -0500

    Added Edit Friends page (only display; no tapping functionality yet)

--- app/src/main/java/com/mitchum/maynard/ribbit/MainActivity.java
+++ app/src/main/java/com/mitchum/maynard/ribbit/MainActivity.java
@@ -129,6 +129,11 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
            ParseUser.logOut();
             navigateToLogin();
         }
+        if(id == R.id.action_edit_friends)
+        {
+            Intent intent = new Intent( this, EditFriendsActivity.class );
+            startActivity( intent );
+        }
         return super.onOptionsItemSelected( item );
     }
 
---
+++ app/src/main/java/com/mitchum/maynard/ribbit/ParseConstants.java
@@ -0,0 +1,9 @@
+package com.mitchum.maynard.ribbit;
+
+public final class ParseConstants
+{
+    //Class name
+
+    //Field names
+    public static final String KEY_USERNAME = "username";
+}
